685ce33a8d7cd5a81f1cf4b37103ba4a699d4187
Maksim <max.1172.luk@gmail.com>
2021-01-20 14:14:38 +0300

    removed gitignore files

--- app/src/main/java/luxs/max/weap7138/Meta.kt
+++
@@ -1,8 +0,0 @@
-package luxs.max.weap7138
-
-class Meta(){
-    companion object{
-        const val KEY = "x-rapidapi-key: 428dc8cd86mshb5771208a2f96b7p1298abjsn6f89d16e8d94"
-        const val HOST = "x-rapidapi-host: community-open-weather-map.p.rapidapi.com"
-    }
-}
